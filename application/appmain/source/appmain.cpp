38bccedb73fcdd0e2829d9252e0804059c3ee0ed
Anand Kathiresan <aillanan@yahoo.com>
2018-07-13 09:37:51 +0530

    Protobuf example is completed with addressbook tutorial

--- application/appmain/source/appmain.cpp
+++ application/appmain/source/appmain.cpp
@@ -4,6 +4,8 @@
 #include "addressbook.pb.h"
 using namespace std;
 
+static const unsigned int READ_MODE = 1;
+static const unsigned int WRITE_MODE = 2;
 // Iterates though all people in the AddressBook and prints info about them.
 void ListPeople(const tutorial::AddressBook& address_book) {
 	for (int i = 0; i < address_book.people_size(); i++) {
@@ -11,9 +13,9 @@ void ListPeople(const tutorial::AddressBook& address_book) {
 
 		cout << "Person ID: " << person.id() << endl;
 		cout << "  Name: " << person.name() << endl;
-		if (person.has_email()) {
+		//if (person.email() != NULL) {
 			cout << "  E-mail address: " << person.email() << endl;
-		}
+		//}
 
 		for (int j = 0; j < person.phones_size(); j++) {
 			const tutorial::Person::PhoneNumber& phone_number = person.phones(j);
@@ -34,6 +36,50 @@ void ListPeople(const tutorial::AddressBook& address_book) {
 	}
 }
 
+// This function fills in a Person message based on user input.
+void PromptForAddress(tutorial::Person* person) {
+  cout << "Enter person ID number: ";
+  int id;
+  cin >> id;
+  person->set_id(id);
+  cin.ignore(256, '\n');
+
+  cout << "Enter name: ";
+  getline(cin, *person->mutable_name());
+
+  cout << "Enter email address (blank for none): ";
+  string email;
+  getline(cin, email);
+  if (!email.empty()) {
+    person->set_email(email);
+  }
+
+  while (true) {
+    cout << "Enter a phone number (or leave blank to finish): ";
+    string number;
+    getline(cin, number);
+    if (number.empty()) {
+      break;
+    }
+
+    tutorial::Person::PhoneNumber* phone_number = person->add_phones();
+    phone_number->set_number(number);
+
+    cout << "Is this a mobile, home, or work phone? ";
+    string type;
+    getline(cin, type);
+    if (type == "mobile") {
+      phone_number->set_type(tutorial::Person::MOBILE);
+    } else if (type == "home") {
+      phone_number->set_type(tutorial::Person::HOME);
+    } else if (type == "work") {
+      phone_number->set_type(tutorial::Person::WORK);
+    } else {
+      cout << "Unknown phone type.  Using default." << endl;
+    }
+  }
+}
+
 // Main function:  Reads the entire address book from a file and prints all
 //   the information inside.
 int main(int argc, char* argv[]) {
@@ -41,26 +87,65 @@ int main(int argc, char* argv[]) {
 	// compatible with the version of the headers we compiled against.
 	GOOGLE_PROTOBUF_VERIFY_VERSION;
 
-	if (argc != 2) {
+	/*if (argc != 2) {
+
 		cerr << "Usage:  " << argv[0] << " ADDRESS_BOOK_FILE" << endl;
 		return -1;
-	}
+	}*/
 
-	tutorial::AddressBook address_book;
+	int mode;
+	cout << "Chose the Mode:\n1. Display the serialized data\n2.Write serialized data" << endl;
+	cin >> mode;
 
-	{
-		// Read the existing address book.
-		fstream input(argv[1], ios::in | ios::binary);
-		if (!address_book.ParseFromIstream(&input)) {
-			cerr << "Failed to parse address book." << endl;
-			return -1;
+	cout << "Enter the proto file name" << endl;
+	string protoFilename;
+	//getline(cin, protoFilename);
+	cin >> protoFilename;
+
+	if (READ_MODE == mode) {
+		tutorial::AddressBook address_book;
+		{
+			// Read the existing address book.
+			fstream input(protoFilename, ios::in | ios::binary);
+			if (!address_book.ParseFromIstream(&input)) {
+				cerr << "Failed to parse address book." << endl;
+				return -1;
+			}
 		}
+		ListPeople(address_book);
+		// Optional:  Delete all global objects allocated by libprotobuf.
+		google::protobuf::ShutdownProtobufLibrary();
 	}
+	else if (WRITE_MODE == mode)
+	{
+		tutorial::AddressBook address_book;
+		{
+			// Read the existing address book.
+			fstream input(protoFilename, ios::in | ios::binary);
+			if (!input) {
+			  cout << protoFilename << ": File not found.  Creating a new file." << endl;
+			} else if (!address_book.ParseFromIstream(&input)) {
+			  cerr << "Failed to parse address book." << endl;
+			  return -1;
+			}
+		}
 
-	ListPeople(address_book);
-
-	// Optional:  Delete all global objects allocated by libprotobuf.
-	google::protobuf::ShutdownProtobufLibrary();
+		// Add an address.
+		PromptForAddress(address_book.add_people());
+		{
+			// Write the new address book back to disk.
+			fstream output(protoFilename, ios::out | ios::trunc | ios::binary);
+			if (!address_book.SerializeToOstream(&output)) {
+			  cerr << "Failed to write address book." << endl;
+			  return -1;
+			}
+		}
+		// Optional:  Delete all global objects allocated by libprotobuf.
+		google::protobuf::ShutdownProtobufLibrary();
+	}
+	else {
+		cout << "Invalid choice" << endl;
+	}
 
 	return 0;
 }
--- buildsystem/scripts/libpath.cmake
+++ buildsystem/scripts/libpath.cmake
@@ -1,12 +1,12 @@
 
 if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
-	set(DEP_LIBS "${CMAKE_CURRENT_SOURCE_DIR}/../../protobuf/proto/source/lib/debug/libprotocd.lib"
-				"${CMAKE_CURRENT_SOURCE_DIR}/../../protobuf/proto/source/lib/debug/libprotobufd.lib"
-				"${CMAKE_CURRENT_SOURCE_DIR}/../../protobuf/proto/source/lib/debug/libprotobuf-lited.lib"
+	set(DEP_LIBS "${CMAKE_CURRENT_SOURCE_DIR}/../../serializedata/address/source/lib/debug/libprotocd.lib"
+				"${CMAKE_CURRENT_SOURCE_DIR}/../../serializedata/address/source/lib/debug/libprotobufd.lib"
+				"${CMAKE_CURRENT_SOURCE_DIR}/../../serializedata/address/source/lib/debug/libprotobuf-lited.lib"
 				 )
 else()
-	set(DEP_LIBS "${CMAKE_CURRENT_SOURCE_DIR}/../../protobuf/proto/source/lib/release/libprotoc.lib"
-				"${CMAKE_CURRENT_SOURCE_DIR}/../../protobuf/proto/source/lib/release/libprotobuf.lib"
-				"${CMAKE_CURRENT_SOURCE_DIR}/../../protobuf/proto/source/lib/release/libprotobuf-lite.lib"
+	set(DEP_LIBS "${CMAKE_CURRENT_SOURCE_DIR}/../../serializedata/address/source/lib/release/libprotoc.lib"
+				"${CMAKE_CURRENT_SOURCE_DIR}/../../serializedata/address/source/lib/release/libprotobuf.lib"
+				"${CMAKE_CURRENT_SOURCE_DIR}/../../serializedata/address/source/lib/release/libprotobuf-lite.lib"
 				)
 endif()
